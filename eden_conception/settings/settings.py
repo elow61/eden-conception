5a8eca672d0393d934400e9fcfba5bb6bb9cbb1d
Elodie Meunier <eme@subteno.com>
2021-07-01 11:16:56 +0200

    [ADD] - Add travis.ci into the project and Sentry SDK

--- eden_conception/settings/settings.py
+++ eden_conception/settings/settings.py
@@ -11,6 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="https://dc40fa2bca5e4e94b682a528d11afef7@o530696.ingest.sentry.io/5842536",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
