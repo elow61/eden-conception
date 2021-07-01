2370f9aae456203f0b2fddcb8269d91a5e0a187d
Elodie Meunier <eme@subteno.com>
2021-07-01 22:23:53 +0200

    [REF] - Refactoring code in project app

--- project/models.py
+++ project/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.db.models import F
 from django.utils.timezone import now
 from user.models import User
+
+
+class ProjectManager(models.Manager):
+
+    def add_member(self, email, project):
+        user = User.objects.filter(email__contains=email)
+        import pdb; pdb.set_trace();
+
+        if not user.exists():
+            return
+
+        project.user_ids.add(user.get().id)
+        return True
 
 
 class Project(models.Model):
@@ -11,6 +25,9 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     number_task = models.IntegerField(default=0)
+
+    objects = models.Manager()
+    objects_project = ProjectManager()
 
     @property
     def get_number_task(self):
@@ -41,18 +58,17 @@ class List(models.Model):
         return self.name
 
 
-class Task(models.Model):
+class TaskManager(models.Manager):
 
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    project_list = models.ForeignKey(List, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
-    deadline = models.DateField(blank=True, null=True)
-    index = models.IntegerField(db_index=True)
-    planned_hours = models.FloatField(default=0)
-    effective_hours = models.FloatField(default=0)
-    remaining_hours = models.FloatField(default=0)
+    def get_index(self, current_list):
+        index = 0
+        if current_list.task_set.all().count() > 0:
+            index = current_list.task_set.all().reverse()[0].index + 1
+
+        next_tasks = Task.objects.filter(index__gte=index)
+        next_tasks.update(index=F('index') + 1)
+
+        return index
 
     def _format_value(self, value):
         if isinstance(value, float) or isinstance(value, int):
@@ -66,17 +82,39 @@ class Task(models.Model):
                 return '-%02d:%02d' % (hours, minutes)
         return '%02d:%02d' % (hours, minutes)
 
-        return value
+    def update_order_task(self, datas):
+        for data in datas:
+            current_task = Task.objects.get(id=data['task_id'])
+            current_task.project_list = List.objects.get(id=data['list_id'])
+            current_task.index = data['index']
+            current_task.save()
+
+
+class Task(models.Model):
+
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    project_list = models.ForeignKey(List, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    deadline = models.DateField(blank=True, null=True)
+    index = models.IntegerField(db_index=True)
+    planned_hours = models.FloatField(default=0)
+    effective_hours = models.FloatField(default=0)
+    remaining_hours = models.FloatField(default=0)
+
+    objects = models.Manager()
+    objects_task = TaskManager()
 
     @property
     def planned_hours_time(self):
-        return self._format_value(self.planned_hours)
+        return Task.objects_task._format_value(self.planned_hours)
 
     @property
     def effective_hours_time(self):
         timesheets = self.timesheet_set.all()
         total_effective_hours = sum(timesheet.unit_hour for timesheet in timesheets)
-        return self._format_value(total_effective_hours)
+        return Task.objects_task._format_value(total_effective_hours)
 
     @property
     def remaining_hours_time(self):
@@ -84,7 +122,7 @@ class Task(models.Model):
         total_effective_hours = sum(timesheet.unit_hour for timesheet in timesheets)
         remaining_hours = self.planned_hours - total_effective_hours
 
-        return self._format_value(remaining_hours)
+        return Task.objects_task._format_value(remaining_hours)
 
     class Meta:
         ordering = ['index']
--- project/views.py
+++ project/views.py
@@ -38,19 +38,15 @@ class ProjectView(View):
     @staticmethod
     def add_member(request):
         res = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                query = request.POST.get('member_email')
-                project = Project.objects.get(pk=request.POST.get('project_name'))
-                user = User.objects.filter(email__contains=query)
+        if request.method == 'POST':
+            query = request.POST.get('member_email')
+            project = Project.objects.get(pk=request.POST.get('project_name'))
+            new_user = Project.objects_project.add_member(query, project)
 
-                if not user.exists():
-                    res['error'] = _('No user email saved in database')
-                else:
-                    project.user_ids.add(user.get().id)
-                    res['user_name'] = user.get().first_name
-        else:
-            redirect('user:login')
+            if new_user:
+                res['user_name'] = user.get().first_name
+            else:
+                res['error'] = _('No user email saved in database')
 
         return JsonResponse(res)
 
@@ -117,102 +113,82 @@ class ListView(View):
     def create_list(request):
         res = {}
         context = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                name = request.POST.get('list_name')
-                project_id = request.POST.get('project_id')
-                project = Project.objects.get(id=project_id)
+        if request.method == 'POST':
+            name = request.POST.get('list_name')
+            project_id = request.POST.get('project_id')
+            project = Project.objects.get(id=project_id)
 
-                new_list = List.objects.create(
-                    name=name,
-                    project=project,
-                )
-                project.save()
+            new_list = List.objects.create(
+                name=name,
+                project=project,
+            )
+            project.save()
 
-                context['new_list'] = new_list
-                context['create_task_form'] = CreateTaskForm
+            context['new_list'] = new_list
+            context['create_task_form'] = CreateTaskForm
 
-                res['list_name'] = name
-                res['list_id'] = new_list.id
-                res['template'] = render_to_string('project/lists/new_list.html', context, request=request)
-            else:
-                res['error'] = _('Please, write a list name.')
+            res['list_name'] = name
+            res['list_id'] = new_list.id
+            res['template'] = render_to_string('project/lists/new_list.html', context, request=request)
         else:
-            res['error'] = _('Connect before create a list')
+            res['error'] = _('Please, write a list name.')
 
         return JsonResponse(res)
 
     @staticmethod
     def delete_list(request):
         res = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                list_id = request.POST.get('list_id')
-                list_to_delete = List.objects.get(id=list_id)
-                project = Project.objects.get(id=list_to_delete.project_id)
+        if request.method == 'POST':
+            list_id = request.POST.get('list_id')
+            list_to_delete = List.objects.get(id=list_id)
+            project = Project.objects.get(id=list_to_delete.project_id)
 
-                list_to_delete.delete()
-                project.save()
+            list_to_delete.delete()
+            project.save()
 
-                res['list_id'] = list_id
-                res['success'] = _('The list has deleted')
-            else:
-                res['error'] = _('The list doesn\'t have deleted')
+            res['list_id'] = list_id
+            res['success'] = _('The list has deleted')
         else:
-            res['error'] = _('Please, connect you.')
+            res['error'] = _('The list doesn\'t have deleted')
 
         return JsonResponse(res)
 
     @staticmethod
     def create_task(request):
         res = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                user = User.objects.get(id=request.user.id)
-                name = request.POST.get('task_name')
-                list_id = request.POST.get('list_id')
-                current_list = List.objects.get(id=int(list_id))
-                project = Project.objects.get(id=current_list.project_id)
+        if request.method == 'POST':
+            user = User.objects.get(id=request.user.id)
+            name = request.POST.get('task_name')
+            list_id = request.POST.get('list_id')
+            current_list = List.objects.get(id=int(list_id))
+            project = Project.objects.get(id=current_list.project_id)
 
-                # Manage the task's index
-                index = 0
-                if current_list.task_set.all().count() > 0:
-                    index = current_list.task_set.all().reverse()[0].index + 1
+            index = Task.objects_task.get_index(current_list)
+            new_task = Task.objects.create(
+                name=name,
+                project_list=current_list,
+                assigned_to=user,
+                deadline=None,
+                index=index,
+                planned_hours=0.0,
+            )
+            project.save()
 
-                next_tasks = Task.objects.filter(index__gte=index)
-                next_tasks.update(index=F('index') + 1)
+            context = {'task': new_task, 'project': project}
 
-                new_task = Task.objects.create(
-                    name=name,
-                    project_list=current_list,
-                    assigned_to=user,
-                    deadline=None,
-                    index=index,
-                    planned_hours=0.0,
-                )
-                project.save()
-
-                context = {'task': new_task, 'project': project}
-
-                res['task_name'] = name
-                res['task_id'] = new_task.id
-                res['list_id'] = list_id
-                res['template'] = render_to_string('project/tasks/new_task.html', context)
+            res['task_name'] = name
+            res['task_id'] = new_task.id
+            res['list_id'] = list_id
+            res['template'] = render_to_string('project/tasks/new_task.html', context)
 
         return JsonResponse(res)
 
     @staticmethod
     def update_order_task(request):
         res = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                datas = json.loads(request.POST.get('datas'))
-
-                for data in datas:
-                    current_task = Task.objects.get(id=data['task_id'])
-                    current_task.project_list = List.objects.get(id=data['list_id'])
-                    current_task.index = data['index']
-                    current_task.save()
+        if request.method == 'POST':
+            datas = json.loads(request.POST.get('datas'))
+            Task.objects_task.update_order_task(datas)
 
         return JsonResponse(res)
 
@@ -229,32 +205,32 @@ class TaskView(View):
         return render(request, self.template_name, context)
 
     def post(self, request, task_id):
+        ''' Method to display the form to update one task '''
         res = {}
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                task_id = request.POST.get('task_id')
-                current_task = Task.objects.get(id=int(task_id))
-                datas = {}
-                datas['name'] = current_task.name
-                datas['assigned_to'] = current_task.assigned_to
-                datas['deadline'] = current_task.deadline
-                datas['description'] = current_task.description
-                datas['planned_hours'] = current_task.planned_hours
+        if request.method == 'POST':
+            task_id = request.POST.get('task_id')
+            current_task = Task.objects.get(id=int(task_id))
+            datas = {}
+            datas['name'] = current_task.name
+            datas['assigned_to'] = current_task.assigned_to
+            datas['deadline'] = current_task.deadline
+            datas['description'] = current_task.description
+            datas['planned_hours'] = current_task.planned_hours
 
-                form_update = UpdateTaskForm(instance=current_task, initial=datas)
-                TimeFormSet = inlineformset_factory(
-                    parent_model=Task,
-                    model=Timesheet,
-                    form=UpdateTimesheetForm,
-                    can_delete=True,
-                    extra=1,
-                    fields=('created_at', 'user', 'description', 'unit_hour')
-                )
-                formset = TimeFormSet(instance=current_task)
-                context = {'form_update': form_update, 'formset': formset, 'task': current_task}
+            form_update = UpdateTaskForm(instance=current_task, initial=datas)
+            TimeFormSet = inlineformset_factory(
+                parent_model=Task,
+                model=Timesheet,
+                form=UpdateTimesheetForm,
+                can_delete=True,
+                extra=1,
+                fields=('created_at', 'user', 'description', 'unit_hour')
+            )
+            formset = TimeFormSet(instance=current_task)
+            context = {'form_update': form_update, 'formset': formset, 'task': current_task}
 
-                res['task_id'] = task_id
-                res['template'] = render_to_string('project/tasks/forms/update_task.html', context, request=request)
+            res['task_id'] = task_id
+            res['template'] = render_to_string('project/tasks/forms/update_task.html', context, request=request)
 
         return JsonResponse(res)
 
@@ -267,32 +243,30 @@ class TaskView(View):
             form=UpdateTimesheetForm,
             can_delete=True,
         )
-        if request.user.is_authenticated:
-            if request.method == 'POST':
-                user = User.objects.get(id=request.POST.get('assigned_to'))
-                date_object = datetime.strptime(request.POST.get('deadline'), '%d/%m/%Y')
-                current_task = Task.objects.get(id=request.POST.get('task_id'))
+        if request.method == 'POST':
+            user = User.objects.get(id=request.POST.get('assigned_to'))
+            date_object = datetime.strptime(request.POST.get('deadline'), '%d/%m/%Y')
+            current_task = Task.objects.get(id=request.POST.get('task_id'))
 
-                formset = TimeFormSet(request.POST, instance=current_task)
+            formset = TimeFormSet(request.POST, instance=current_task)
 
-                if formset.is_valid():
-                    formset.save()
+            if formset.is_valid():
+                formset.save()
 
-                # Convert time to float
-                convert_in_time = datetime.strptime(request.POST.get('planned_hours'), '%H:%M').time()
-                planned_hours_float = convert_in_time.hour + convert_in_time.minute / 60.0
+            # Convert time to float
+            convert_in_time = datetime.strptime(request.POST.get('planned_hours'), '%H:%M').time()
+            planned_hours_float = convert_in_time.hour + convert_in_time.minute / 60.0
 
-                current_task.name = request.POST.get('name')
-                current_task.assigned_to = user
-                current_task.deadline = date_object
-                current_task.description = request.POST.get('description')
-                current_task.planned_hours = planned_hours_float
-                current_task.save()
+            current_task.name = request.POST.get('name')
+            current_task.assigned_to = user
+            current_task.deadline = date_object
+            current_task.description = request.POST.get('description')
+            current_task.planned_hours = planned_hours_float
+            current_task.save()
 
-                
-                context = {'task': current_task}
+            context = {'task': current_task}
 
-                res['task_id'] = current_task.id
-                res['template'] = render_to_string('project/tasks/task_detail.html', context)
+            res['task_id'] = current_task.id
+            res['template'] = render_to_string('project/tasks/task_detail.html', context)
 
         return JsonResponse(res)
