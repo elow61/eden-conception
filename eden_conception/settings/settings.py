b026d205944d52ebc6bd8f54b9ff0b18ac1f1e09
Elodie Meunier <eme@subteno.com>
2021-07-01 12:26:52 +0200

    [FIX] - Fix change configuration

--- eden_conception/settings/settings.py
+++ eden_conception/settings/settings.py
@@ -30,7 +30,7 @@ sentry_sdk.init(
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
